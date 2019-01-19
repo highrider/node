a8074d0cbcccd90bf8b9c1c513fe503659c85faf
highriderr <moremorejoy@gmail.com>
2019-01-19 14:49:52 +0300

    my first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express(); //вот здесь и начинается наше приложение, здесь мы запустили так сказать конструктор
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public')); //здесь мы вставляем статическую страницу, с уже написанным html  кодом
+
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {  //здесь, / значит, корневая папка сайта например tut.by. req and res  имеют свои методы
+    // res.send('<h1>hello express</h1>'); //это ответ на запрос
+    res.render('home.hbs' , {
+        pageTitle: "This is home page",
+        welcomeMessage: 'Hello user you in my first server app'
+    });
+});
+
+app.get('/about', (req, res) => { //здесь мы делаем запрос на наш новый роут /about  только через метод render, который подтягивает шаблон
+    res.render('about.hbs', {
+        pageTitle: "About page"
+    });
+})
+
+app.get('/bad', (req,res) => {
+    res.send({
+        error: 'this is 404 error',
+        errorMessage: 'this type of error may occur if you have some request page problems'
+    });
+});
+
+//но это все безсмысленно пока мы не установим обработчик на событие app.get()
+//сделаем это
+app.listen(3000, () => {
+    console.log('server now is set up');
+});  //биндим этот обработчик на порт на нашей машине
